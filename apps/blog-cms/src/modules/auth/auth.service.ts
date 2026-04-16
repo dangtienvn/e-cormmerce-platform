8b9ab2b381839c04a3bd0910e9489e276832e1c9
dangtienvn <td2812009@gmail.com>
2026-04-16 10:46:51 +0000

    feat(blog): implement auth.service.ts

---
+++ apps/blog-cms/src/modules/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { prisma } from '@/lib/prisma';
+
+export const getUserByEmail = async (email: string) => {
+    return await prisma.user.findUnique({
+        where: { email }
+    });
+};
+
+export const getUserById = async (id: number) => {
+    return await prisma.user.findUnique({
+        where: { id }
+    });
+};
